c58a85b10be43a64cf0b76e7f74fe532b0b8c3ab
youzaiyouzai666 <1620030379@qq.com>
2017-05-05 21:09:23 +0800

    闭关32天

--- index.js
+++ index.js
@@ -1,29 +1,49 @@
 import React, {Component, PropTypes} from 'react';
 import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
-const reducer = function (state = 0, action) {
+import {Provider, connect} from 'react-redux';
+
+const reducer = function (state = {count:0}, action) {
+    const count = state.count;
     switch (action.type) {
         case 'ADD':
-            return ++state;
+            return Object.assign({},state,{count: count+1});
         default:
             return state; //getState是通过这个获得初始值的
     }
 };
 const store   = createStore(reducer);
+
+const addAction = {type: 'ADD'};
 class Counter extends Component {
     render() {
-        const {value} = this.props;
+        const {value,onAdd} = this.props;
         return (
             <div>
-                <span>{store.getState()}</span>
-                <button onClick={()=>store.dispatch({type: 'ADD'})}>Increase</button>
+                <span>{value}</span>
+                <button onClick={onAdd}>Increase</button>
             </div>
         )
     }
 }
+function mapStateTopProps(state){
+    return {
+        value: state.count
+    }
+}
+function mapDispatchToProps(dispatch){
+    return {
+        onAdd: ()=>dispatch(addAction)
+    }
+}
+const App = connect(
+    mapStateTopProps,
+    mapDispatchToProps
+)(Counter);
 const render = () => ReactDOM.render(
-    <Counter value="1"/>,
+    <Provider store={store}>
+        <App/>
+    </Provider>,
     document.getElementById('root')
 );
 render();
-store.subscribe(render);
